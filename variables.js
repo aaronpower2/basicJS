839c23e84a350f9512429f8611975e0d766a2651
Aaron Power <aaronpower2@gmail.com>
2021-09-06 07:10:30 +0400

    experiments with arrays

--- variables.js
+++ variables.js
@@ -1,20 +1,35 @@
-//adding
+// //adding
+// let number = 8
+// const two = 2
 
-let number = 8
-const two = 2
+// number += two
 
-number += two
+// console.log(number)
+// console.log("-----------------")
 
-console.log(number)
+// //incrementing
+// number ++
+// console.log(number)
+// console.log("-----------------")
 
-//incrementing
-
-number ++
-console.log(number)
+// create a random number
+function randNum() {
+    return Math.floor(Math.random() * 100)
+}
+console.log(randNum())
 
 //init array
-
 let myArray = [1,2,3,4,5]
 let arrayLen = myArray.length
 
-console.log(arrayLen)
+//reverse array items
+myArray.reverse()
+
+let i = 0
+let arraySum = 0
+
+while (i < arrayLen) {
+    arraySum += myArray[i]
+    i++
+    console.log(arraySum)
+}
