af6b438c046ac1c6911cceee6df1d4060d8746cb
Aaron Power <aaronpower2@gmail.com>
2021-09-06 06:42:46 +0400

    variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+//adding
+
+let number = 8
+const two = 2
+
+number += two
+
+console.log(number)
+
+//incrementing
+
+number ++
+console.log(number)
+
+//init array
+
+let myArray = [1,2,3,4,5]
+let arrayLen = myArray.length
+
+console.log(arrayLen)
